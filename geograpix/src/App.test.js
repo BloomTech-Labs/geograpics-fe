37878711a5c04e39906ec667d2d4e8cad6f20777
anthonypiazza <piazza.anthony.n@gmail.com>
2019-09-20 15:16:49 -0500

    Added Enzyme config, setupTests file, ran eject script, added initial test for static title, more to come

--- geograpix/src/App.test.js
+++ geograpix/src/App.test.js
@@ -3,23 +3,55 @@ import toJson from 'enzyme-to-json';
 import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
 import App from './App';
-import Map from './components/Map';
+import { Map } from './components/Map';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
 
-describe('Map', () => {
-  it('displays map starting point based on attribute data', () => {
-    const map = shallow(
-      <Map 
-        pictureInfo = "This is picture info"
-      />
-    )
-    expect(map.find().pictureInfo()).toBe('This is picture info');
-    pictureInfo.setProps({ pictureInfo: "New picture info" })
-    expect(map.find().pictureInfo()).toBe('New picture info')
+const mockPictureInfo = [
+  {
+    media_id: 22721881,
+    user_id: 1,
+    longitude: -118.3852043,
+    latitude: 33.9650947,
+    thumbnail: "https://i.ibb.co/qR7ST7G/image.png",
+    standard_resolution: "https://i.ibb.co/mHbjfbs/image.png",
+    created_time: 1279340983,
+    caption: "this is a test",
+    likes: 10,
+  },
+];
+  
+let wrapper = shallow(<Map pictureInfo={mockPictureInfo} />)
+
+
+describe('When the map is rendered', () => {
+  it('should render title', () => {
+    expect(wrapper.find('.title').text()).toEqual('This is map');
   })
 })
+
+// app/src/components/__tests__/Login-test.js
+// describe('Map Component', () => {
+//   // our mock login function to replace the one provided by mapDispatchToProps
+//   const mockLoginfn = jest.fn();
+//   beforeEach(() => {
+//     map = shallow(<Map pictureInfo={mockLoginfn}/>)
+//   })
+//   expect
+// }
+
+// // app/src/components/__tests__/Login-test.js\
+// describe('When the form is submitted', () => {
+//   it('should call the mock login function', () => {
+//     wrapper.find('#loginForm').simulate(
+//       'submit', 
+//       {preventDefault() {}}
+//     )
+//     expect(mockLoginfn.mock.calls.length).toBe(1)
+//    })
+//  })
+
