d1afba5f7cf1e4f7689f5d51acdb65405074f32b
anthonypiazza <piazza.anthony.n@gmail.com>
2019-09-20 20:02:41 -0500

    Local Storage tweak

--- geograpix/src/App.js
+++ geograpix/src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <Route exact path="/" component={Register} />
-      <Route exact path="/preloader" render={(props) => <Preloader {...props} />} />
+      <Route exact path="/preloader/" component={Preloader} />
       <Route path="/register/2/" component={Register2} />
       <section class="map">
         <header class="App-header">
