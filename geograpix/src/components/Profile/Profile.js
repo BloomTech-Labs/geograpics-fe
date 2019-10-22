965be3263c29d599251ac799063c7ac264b184c9
kingmoc <joe.c.a.garcia@gmail.com>
2019-10-22 01:07:20 -0500

    Updated Profile to Match Mocks

--- geograpix/src/components/Profile/Profile.js
+++ geograpix/src/components/Profile/Profile.js
@@ -4,37 +4,40 @@ import PropTypes from 'prop-types';
 export const Profile = props => {
   // console.log('PROFILE PROPS',props);
 
+  const untagged = props.pictureInfo.pictures.filter(picture => !picture.latitude)
+
   return(
       <div className="profile-tab-box">
         <div className="profile-tab-top-div">
           <div className="profile-tab-img-div">
             <img className="profile-tab-prof-pic" src= {props.pictureInfo.profile_pic} alt= {props.pictureInfo.username}/>
+            <div className="bold-cont">
+              <p className="top-div-details-bold">Edit Profile</p>
+              <p className="top-div-details-bold">Privacy</p>
+            </div>
           </div>
           <div className="top-div-details-div">
-            <h5 className="top-div-details-name">{props.pictureInfo.full_name}</h5>
+            <h5 className="top-div-details-name">{props.pictureInfo.username}</h5>
             <p className="top-div-details">{props.pictureInfo.email}</p>
-            <p className="top-div-details-bold">Edit Profile</p>
-            <p className="top-div-details-bold">Privacy Settings</p>
+            <div className="profile-tab-middle-div">
+              <div>
+                <p className="middle-div-details">{props.pictureInfo.pictures.length - untagged.length} <span>Gpost</span></p>
+              </div>
+              <div>
+                <p className="middle-div-details">{untagged.length} <span>Untagged Photos</span></p>
+              </div>
+            </div>
+            <h6>{props.pictureInfo.full_name}</h6>
+            <p>{props.pictureInfo.bio}</p>
+            <p className='web'>{props.pictureInfo.website}</p>
+            <div className="profile-tab-bottom-div">
+              <button className="btn-signout" onClick={props.logout}>Sign Out</button>
+            </div>
           </div>
         </div>
-        <div className="profile-tab-middle-div">
-          <div>
-            <p className="middle-div-details">{props.pictureInfo.pictures.length}</p>
-            <p className="middle-div-details">Posts</p>
-          </div>
-          <div>
-            <p className="middle-div-details">50</p>
-            <p className="middle-div-details">Followers</p>
-          </div>
-          <div>
-            <p className="middle-div-details">{props.pictureInfo.pictures.length}</p>
-            <p className="middle-div-details">Posts</p>
-          </div>
-        </div>
-        <div className="profile-tab-bottom-div">
-            <button className="btn-instagramaccount">Instagram Account</button>
+        {/* <div className="profile-tab-bottom-div">
             <button className="btn-signout" onClick={props.logout}>Sign Out</button>
-        </div>
+        </div> */}
       </div>
   );
 };
