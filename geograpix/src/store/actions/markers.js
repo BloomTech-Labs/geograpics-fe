c50fba23d62711f3532231401dbb7413190a59e1
bwpeck <benjaminwade483@yahoo.com>
2020-02-20 19:41:34 -0500

    try it

--- geograpix/src/store/actions/markers.js
+++ geograpix/src/store/actions/markers.js
@@ -5,13 +5,13 @@ export const GET_PICTURE_OBJECT_SUCCESS = "GET_PICTURE_OBJECT_SUCCESS";
 export const GET_PICTURE_OBJECT_FAIL = "GET_PICTURE_OBJECT_FAIL";
 
 // const hostURL = `//geograpics-staging.herokuapp.com`;
-const hostURL = process.env.REACT_APP_URL || '//localhost:8000'
+const hostURL = process.env.REACT_APP_URL || '//localhost:8000'jsssad
 
 export const getPictureObject = () => dispatch => {
   console.log('GET_PICTURE_OBJECT_START')
   dispatch({ type: GET_PICTURE_OBJECT_START });
   axiosWithAuth()
-    .get(`${hostURL}/map/update`)
+    .get(`${hostURL}/map`)
         .then(res => {
           dispatch({ type: GET_PICTURE_OBJECT_SUCCESS, payload: res.data });
           console.log('GET_PICTURE_OBJECT_SUCCESS', res.data)
